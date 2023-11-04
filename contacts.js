fd03a85d30876895e4ef6dc137796dc45133a759
Marini23 <marina.smile1m@gmail.com>
2023-11-04 12:14:44 +0100

    Initial commit

---
+++ contacts.js
@@ -0,0 +1,48 @@
+import { nanoid } from "nanoid";
+import fs from "fs/promises";
+import path from "path";
+
+const contactsPath = path.resolve("db", "contacts.json");
+
+const updateContacts = (contacts) =>
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+export async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+export async function getContactById(id) {
+  const contacts = await listContacts();
+  const contactById = contacts.find((contact) => contact.id === id);
+  return contactById || null;
+}
+
+export async function removeContact(id) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === id);
+  if (index === -1) {
+    return null;
+  }
+  const [removeContact] = contacts.splice(index, 1);
+  await updateContacts(contacts);
+  return removeContact;
+}
+
+export async function addContact({ name, email, phone }) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await updateContacts(contacts);
+  return newContact;
+}
---
+++ index.js
@@ -0,0 +1,37 @@
+import yargs from "yargs";
+import * as contactsservice from "./contacts.js";
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contactsservice.listContacts();
+      console.log(allContacts);
+      break;
+
+    case "get":
+      const oneContact = await contactsservice.getContactById(id);
+      console.log(oneContact);
+      break;
+
+    case "add":
+      const newContact = await contactsservice.addContact({
+        name,
+        email,
+        phone,
+      });
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const removeContact = await contactsservice.removeContact(id);
+      console.log(removeContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+const { argv } = yargs(process.argv.splice(2));
+
+invokeAction(argv);
